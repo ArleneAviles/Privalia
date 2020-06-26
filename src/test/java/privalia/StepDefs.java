0f40b1aa1b7333e649ebe5a57063904804f05327
Arlene Aviles <arlene.aviles@astrazeneca.com>
2020-06-26 11:49:41 -0500

    Codigo de Privalia

--- src/test/java/privalia/StepDefs.java
+++ src/test/java/privalia/StepDefs.java
@@ -4,12 +4,16 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class StepDefs {
@@ -35,21 +39,56 @@ public class StepDefs {
 
     @Given("I am in Privalia Home Page")
     public void iAmInPrivaliaHomePage() {
+        driver.get("https://mex.privalia.com/");
 
+        //encontrar el elemento banner de privalia
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("claim"))).isDisplayed();
     }
 
 
     @Then("All the featured ads are up to date")
     public void allTheFeaturedAdsAreUpToDate() {
+        List<WebElement> destacados = driver.findElements(By.xpath("//h2[ text() = 'Destacados']/following-sibling::article"));
+
+        for(WebElement destacado: destacados){
+            String campanaElementLink = driver.findElement(By.cssSelector(".preopen-info a")).getAttribute("href");
+            System.out.println(campanaElementLink);
+            String campañaArticulo = destacado.getAttribute("data-prv-campaignname");
+            String artEnCampanacampaña = campañaArticulo.replaceAll(" ","-");
+            campanaElementLink.contains(campañaArticulo);
+            System.out.println(campañaArticulo);
+            WebElement vigencia = destacado.findElement(By.cssSelector(".item-dataInfo"));
+
+/*            if (vigencia.getText().isEmpty())
+            {
+                System.out.println(vigencia.getText());
+            }else
+                vigencia.getText();*/
+ //           try{
+ //               System.out.println(vigencia.getText());
+ //           }catch(Exception e){
+//            }
+        }
 
     }
 
     @Then("All the current ads are up to date")
     public void allTheCurrentAdsAreUpToDate() {
+        List<WebElement> estrenados = driver.findElements(By.xpath("//h2[ text() = 'Hoy Estrenados']/following-sibling::article"));
 
+        for (WebElement estrenado : estrenados) {
+            WebElement vigencia = estrenado.findElement(By.cssSelector(".item-dataInfo"));
+            System.out.println(vigencia.getText());
+        }
     }
 
     @Then("Coming up promotions don´t have a due date")
     public void comingUpPromotionsDonTHaveADueDate() {
+            List<WebElement> futuros = driver.findElements(By.xpath("//h2[ text() = 'Próximamente']/following-sibling::article"));
+
+            for(WebElement futuro: futuros) {
+                WebElement vigencia = futuro.findElement(By.cssSelector(".item-dataInfo"));
+                System.out.println(vigencia.getText());
+            }
     }
 }
--- src/test/java/privalia/privaliaPromociones.feature
+++ src/test/java/privalia/privaliaPromociones.feature
@@ -1,14 +1,14 @@
 Feature:  promociones home page
 
-  Scenario: Featred promotions are up to date
+  Scenario: Featured promotions are up to date
     Given I am in Privalia Home Page
     Then All the featured ads are up to date
 
-  Scenario: Currente promotions are up to date
-    Given I am in Privalia Home Page
-    Then All the current ads are up to date
+#  Scenario: Current promotions are up to date
+#    Given I am in Privalia Home Page
+#    Then All the current ads are up to date
 
-  Scenario: Comming up promotions don´t have due date
-    Given I am in Privalia Home Page
-    Then Coming up promotions don´t have a due date
+ # Scenario: Coming up promotions don´t have due date
+ #   Given I am in Privalia Home Page
+ #   Then Coming up promotions don´t have a due date
 
